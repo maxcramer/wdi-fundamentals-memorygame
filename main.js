5c13c143bf77c0e426e03a82e2ca1bcef46fcff1
maxcramer <39188094+maxcramer@users.noreply.github.com>
2018-06-01 15:03:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+var cardOne = [{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+}];
+var cardTwo = [{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+}];
+var cardThree = [{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+}];
+var cardFour = [{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}];
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+var cardsInPlay.push["cardOne", "cardTwo"];
+function flipCard () {
+	var cardId = this.getAttribute('data-id');
+
+this.setAttribute('src').cards[cardId].cardimage;
+
+if (length) {
+	cardsInPlay === [2]window.alert("You found a match");
+} else {
+	cardsInPlay[0]window.alert("Sorry, try again");
+};
+console.log("User flipped" + cards[cardId].rank);
+};
+function createBoard 
+for (var i = 0; i < cardOne.length; i++) {
+}
+for (var i = 0; i < cardTwo.length; i++) {
+}
+for (var i = 0; i < cardThree.length; i++) {
+}
+for (var i = 0; i < cardFour.length; i++) {
+
+var cardElement = document.createElement('img');
+
+cardElement.setAttribute('src','images/back.png');
+
+cardElement.setAttribute[i]('data-id');
+
+addEventListener(click).cardElement(flipCard);
+
+cardElement.appendChild(game-board);
+
+};
+
+cardsInPlay.push(cards[cardId].rank);
+console.log('cardImage');
+console.log('suit');
+function checkForMatch();
+return 0, 2;
+function checkForMatch{if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+};
+}; 
+
+function createBoard ();
